87bdebe92a09073bbf00ec72c682349663a58b5a
Clint Johnson <Clint.Johnson.07@gmail.com>
2018-07-24 21:14:10 -0400

    Adding some css

--- application.py
+++ application.py
@@ -146,6 +146,27 @@ def gconnect():
     return output
 
 
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User_info).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
 @app.route('/gdisconnect')
 def gdisconnect():
     access_token = login_session.get('access_token')
